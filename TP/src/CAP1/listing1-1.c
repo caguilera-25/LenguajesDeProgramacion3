aaeeae9e9edfb7b16f6c780f90cd9759999ff32d
caguilera-25 <caguilera25@LenovoDeCarlos2503.localdomain>
2025-08-27 11:59:48 +0000

    Genere el Listing 1.1, 1.2 y 1.3

---
+++ TP/src/CAP1/listing1-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listing1-3.hpp"
+int main (int argc, char **argv)
+{
+ int i;  i = atoi (argv[1]);
+ printf ("The reciprocal of %d is %g\n", i, reciprocal (i));
+ return 0;
+} 
